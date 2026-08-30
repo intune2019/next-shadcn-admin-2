07a65ff4b03cb9877854752410bea6a42554c9e9
In.Tune & Associates Inc. <admin@intune.inc>
2026-08-29 19:19:40 -0600

    Add files via upload

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,44 @@
-"use client";
-
 import Link from "next/link";
 
+import { CompassIcon, HomeIcon } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyTitle,
+} from "@/components/ui/empty";
 
 export default function NotFound() {
   return (
-    <div className="flex h-dvh flex-col items-center justify-center space-y-2 text-center">
-      <h1 className="font-semibold text-2xl">Page not found.</h1>
-      <p className="text-muted-foreground">The page you are looking for could not be found.</p>
-      <Link prefetch={false} replace href="/dashboard/default">
-        <Button variant="outline">Go back home</Button>
-      </Link>
+    <div className="relative flex min-h-dvh w-full items-center justify-center overflow-hidden">
+      <Empty>
+        <EmptyHeader>
+          <EmptyTitle className="font-extrabold text-9xl">404</EmptyTitle>
+          <EmptyDescription className="-mt-8 text-nowrap text-foreground/80">
+            The page you&apos;re looking for might have been <br />
+            moved or doesn&apos;t exist.
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
+          <div className="flex gap-2">
+            <Button asChild>
+              <Link href="/">
+                <HomeIcon className="mr-2 size-4" data-icon="inline-start" />
+                Go Home
+              </Link>
+            </Button>
+            <Button asChild variant="outline">
+              <Link href="/frontend">
+                <CompassIcon className="mr-2 size-4" data-icon="inline-start" />
+                Explore
+              </Link>
+            </Button>
+          </div>
+        </EmptyContent>
+      </Empty>
     </div>
   );
 }
